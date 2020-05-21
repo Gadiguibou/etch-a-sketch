cbbd109b2a4eccc9f7da2e7e666ef6c4b22aecf5
Gabriel Lacroix <lacroixgabriel@gmail.com>
2020-05-21 18:54:53 -0400

    Add class child-div to divs

--- scripts.js
+++ scripts.js
@@ -3,5 +3,6 @@ const containerDiv = document.querySelector(".container");
 for (let i = 0; i < (16 * 16); i++) {
   const childDiv = document.createElement("div");
   childDiv.textContent = "div";
+  childDiv.classList.add("child-div");
   containerDiv.append(childDiv);
 }
