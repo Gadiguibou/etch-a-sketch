07cdfdf5ceb0879946515ad06f40529912a8288d
Gabriel Lacroix <lacroixgabriel@gmail.com>
2020-05-21 19:06:09 -0400

    Add event listener to child-divs

--- scripts.js
+++ scripts.js
@@ -5,3 +5,10 @@ for (let i = 0; i < (16 * 16); i++) {
   childDiv.classList.add("child-div");
   containerDiv.append(childDiv);
 }
+
+const childDivNodeList = document.querySelectorAll(".child-div");
+childDivNodeList.forEach((childDiv) => {
+  childDiv.addEventListener("pointerenter", () => {
+    console.log(childDiv);
+  });
+});
