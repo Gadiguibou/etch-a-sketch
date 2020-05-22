f2d13267dd8e4484aec02dea1a9bb924ab8c7e34
Gabriel Lacroix <lacroixgabriel@gmail.com>
2020-05-21 21:46:05 -0400

    Refactor to use functions

--- scripts.js
+++ scripts.js
@@ -1,27 +1,44 @@
 const containerDiv = document.querySelector(".container");
 
-for (let i = 0; i < 16 * 16; i++) {
-  const childDiv = document.createElement("div");
-  childDiv.classList.add("child-div");
-  containerDiv.append(childDiv);
-}
+const createGrid = function (size = 16) {
+  const clearContainer = function () {
+    while (containerDiv.firstChild) {
+      containerDiv.removeChild(containerDiv.firstChild);
+    }
+  }
+  clearContainer();
 
-function makeInteractive() {
-  const childDivNodeList = document.querySelectorAll(".child-div");
+  const createGridTemplate = function () {
+    containerDiv.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    containerDiv.style.gridTemplateRows = `repeat(${size},1fr)`
+  }
+  createGridTemplate();
 
-  childDivNodeList.forEach((childDiv) => {
-    childDiv.addEventListener("pointerenter", () => {
-      childDiv.classList.add("active-div");
+  const fillGrid = function () {
+    for (let i = 0; i < size * size; i++) {
+      const childDiv = document.createElement("div");
+      childDiv.classList.add("child-div");
+      containerDiv.append(childDiv);
+    }
+  }
+  fillGrid();
+
+  const makeInteractive = function () {
+    const childDivNodeList = document.querySelectorAll(".child-div");
+  
+    childDivNodeList.forEach((childDiv) => {
+      childDiv.addEventListener("pointerenter", () => {
+        childDiv.classList.add("active-div");
+      });
+      childDiv.addEventListener("pointerout", () => {
+        setTimeout(function () {
+          childDiv.classList.remove("active-div");
+        }, 2000);
+      });
     });
-    childDiv.addEventListener("pointerout", () => {
-      setTimeout(function () {
-        childDiv.classList.remove("active-div");
-      }, 2000);
-    });
-  });
+  }
+  makeInteractive();
 }
-
-makeInteractive();
 
 const resizeBtn = document.querySelector(".resize-button");
 
@@ -36,21 +53,6 @@ resizeBtn.addEventListener("click", function resizeGrid() {
     alert("The number you choose must be greater than 0.");
     return;
   } else {
-    while (containerDiv.firstChild) {
-      containerDiv.removeChild(containerDiv.firstChild);
-    }
-
-    containerDiv.style.gridTemplateColumns = `repeat(${Number(
-      sizePrompt
-    )},1fr)`;
-    containerDiv.style.gridTemplateRows = `repeat(${Number(sizePrompt)},1fr)`;
-
-    for (let i = 0; i < Number(sizePrompt) ** 2; i++) {
-      const childDiv = document.createElement("div");
-      childDiv.classList.add("child-div");
-      containerDiv.append(childDiv);
-    }
-
-    makeInteractive();
+    createGrid(Number(sizePrompt));
   }
 });
