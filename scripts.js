76459ade56f890323736f350d478c094dcc1ffe4
Gabriel Lacroix <lacroixgabriel@gmail.com>
2020-05-21 21:14:31 -0400

    Change color on hover

--- scripts.js
+++ scripts.js
@@ -9,6 +9,9 @@ for (let i = 0; i < (16 * 16); i++) {
 const childDivNodeList = document.querySelectorAll(".child-div");
 childDivNodeList.forEach((childDiv) => {
   childDiv.addEventListener("pointerenter", () => {
-    console.log(childDiv);
+    childDiv.classList.add("active-div");
   });
+  childDiv.addEventListener("pointerout", () => {
+    setTimeout(function () { childDiv.classList.remove("active-div") }, 2000);
+  })
 });
